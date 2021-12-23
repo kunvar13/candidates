159d1c28a88ce2bc92c32b788a1335d0554b243c
kunvar13 <kunvar13@gmail.com>
2021-12-22 22:21:56 -0600

    First commit

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 # This is the main entrypoint into the program
 # It requires the other files/gems that it needs
 require 'pp'
-require './candidates'
-require './filters'
+require '/Users/aadikunvar/lighthouse/w8/candidates/5b77aa4c83e017944cc3f9fae51b9e3f/candidates.rb'
+require '/Users/aadikunvar/lighthouse/w8/candidates/5b77aa4c83e017944cc3f9fae51b9e3f/filters'
 
 ## Your test code can go here
 
